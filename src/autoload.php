5f5f76e1499d0559aaf28c8f5cbc4c0659a5e1d8
IvanElTrolazo24 <carlos050199@gmail.com>
2017-03-31 09:25:05 -0700

    cambio de directorios
    app, src, autoloader

---
+++ src/autoload.php
@@ -0,0 +1,19 @@
+<?php
+return
+//$usuario = new App\Models\Usuario;
+	spl_autoload_register(function($classname)
+	{	
+		//$classname = 'App\Models\Usuario'
+		$ruta = strtolower($classname);
+		//$classname = 'app\models\usuario'
+		$ruta = str_replace("\\", "/", $ruta) . ".php";
+		//$classname = 'app/models/usuario'
+		if (is_readable($ruta)) {
+			require $ruta;
+		}
+		else
+		{
+			die("El archivo $ruta no existe.");
+		}
+		
+	});
